3929da9cad1abf3ed043e9511a2763ca70bd3b5d
Aingkaran <aingkaran1995@gmail.com>
2023-03-07 20:05:22 -0500

    integrate Alchemy SDK

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Network, Alchemy } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState(false);
+  const [ownerAddress, setOwnerAddress] = useState("");
+
+  const config = {
+    apiKey: "Lc65gGXOYJ1LANkb3m83-uZ2_AhJUxXn",
+    network: Network.ETH_MAINNET,
+  };
+
+  const alchemy = new Alchemy(config);
+
+  const main = async (address) => {
+    // Get all NFTs
+    const nfts = await alchemy.nft.getNftsForOwner(address);
+    // Print NFTs
+    console.log(nfts);
+  };
+  const runMain = async (address) => {
+    try {
+      await main(address);
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+
+
+  useEffect(() => {
+
+
+  }, [search])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(ownerAddress)
+    runMain(ownerAddress)
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+    setOwnerAddress(e.target.value)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        NFT Owner Name
+        <input type="text" value={ownerAddress} onChange={handleChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
